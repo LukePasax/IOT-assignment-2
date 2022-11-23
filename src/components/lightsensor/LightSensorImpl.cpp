781a3b2fcd49335b9f117e69d344888048707275
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-11-23 15:56:17 +0100

    remove usless code

--- src/components/lightsensor/LightSensorImpl.cpp
+++ src/components/lightsensor/LightSensorImpl.cpp
@@ -3,7 +3,5 @@
 
 int LightSensorImpl::getLight(){
 int value = analogRead(this->getPin());
-  double valueInVolt = ((double) value) * 5/1024;
-  //Serial.println(String(value) + " -> in volt: " + valueInVolt );
   return value;
 }
