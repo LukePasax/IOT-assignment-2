0a5897f7929b0d051c015b5d2235dd196b1fb830
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-11-28 21:23:22 +0100

    Update graph.py

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -5,37 +5,19 @@ plt.ion()
 fig=plt.figure()
 
 
-<<<<<<< Updated upstream
 x=[]
 y=[]
-=======
 i=0
-x=list()
-y=list()
->>>>>>> Stashed changes
-i=0
-ser = serial.Serial("COM3", 9600)
+ser = serial.Serial("/dev/cu.usbmodem14201", 9600)
 ser.close()
 ser.open()
 while True:
-
     data = ser.readline()
-<<<<<<< Updated upstream
 
     x.append(i)
     y.append(float(data.decode()))
     plt.plot(x,y, 'b')
     plt.axis([-0.001, i, 0, 4])
-=======
-    #print(data.decode())
-    x.append(i)
-    y.append(data.decode())
-    plt.plot(x, y)
-    # naming the x axis
-    plt.xlabel('x - axis')
-    # naming the y axis
-    plt.ylabel('y - axis')
->>>>>>> Stashed changes
     i += 1
     plt.show()
     plt.pause(0.5)
