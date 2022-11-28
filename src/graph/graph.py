aaa023da405eed366c31ba0b20403df72902f3e8
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-11-28 14:37:24 +0100

    close plot and close program

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -9,17 +9,21 @@ i=0
 x=list()
 y=list()
 i=0
-ser = serial.Serial('COM3',9600)
-ser.close()
-ser.open()
+ser = serial.Serial('/dev/cu.usbmodem14201',9600)
+#ser.close()
+#ser.open()
 while True:
 
     data = ser.readline()
-    print(data.decode())
+    #print(data.decode())
     x.append(i)
     y.append(data.decode())
 
     plt.scatter(i, float(data.decode()))
     i += 1
     plt.show()
-    plt.pause(0.0001)  # Note this correction
+    plt.pause(1)  # Note this correction
+    #if i close the plot, close the program
+    if plt.fignum_exists(fig.number) == False:
+        break  
+    
